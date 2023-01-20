9361aba741e328846166ab0b455ca8049ed6183c
ASieSer <a_alejandro.sierra.serrano@iespablopicasso.es>
2023-01-20 14:20:58 +0100

    Añado mis primeros ficheros

---
+++ Programa_EGit/src/EjemploArrayAleatorio.java
@@ -0,0 +1,22 @@
+
+public class EjemploArrayAleatorio {
+
+  public static void main(String[] args) {
+    
+    final int LARGOARRAY = 20;
+    final int INICIOINTERVALO = 1;
+    final int FININTERVALO = 10;
+    int[] numeros = new int[LARGOARRAY];
+    int i = 0;
+    
+    for (i = 0; i < LARGOARRAY; i++) {
+      numeros[i] = (int)(Math.random() * (FININTERVALO - INICIOINTERVALO + 1) + INICIOINTERVALO);
+    }
+
+    for(i = 0; i < LARGOARRAY; i++) {
+      System.out.print(numeros[i] + " ");
+    }
+    
+  }
+
+}
