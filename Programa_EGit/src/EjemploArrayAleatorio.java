9c48fc8e663d19d4652e22555a49127c23fce39c
ASieSer <a_alejandro.sierra.serrano@iespablopicasso.es>
2023-01-27 10:56:19 +0100

    Añado método CuentaRepeticiones

--- Programa_EGit/src/EjemploArrayAleatorio.java
+++ Programa_EGit/src/EjemploArrayAleatorio.java
@@ -7,14 +7,26 @@ public class EjemploArrayAleatorio {
     final int INICIOINTERVALO = 1;
     final int FININTERVALO = 10;
     int[] numeros = new int[LARGOARRAY];
+    int[] repeticiones = new int[20];
     int i = 0;
     
     for (i = 0; i < LARGOARRAY; i++) {
       numeros[i] = (int)(Math.random() * (FININTERVALO - INICIOINTERVALO + 1) + INICIOINTERVALO);
+      if (numeros[i] > 0) {
+        if (numeros[i] == numeros[i]) {
+          repeticiones[numeros[i]]++;  
+        }
+      }
     }
 
     for(i = 0; i < LARGOARRAY; i++) {
       System.out.print(numeros[i] + " ");
+    }
+    
+    System.out.println();
+    
+    for(i = 0; i < LARGOARRAY; i++) {
+      System.out.println("El número " + numeros[i] + " se repite " + repeticiones[i] + " veces.");
     }
     
   }
